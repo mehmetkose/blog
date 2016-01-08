ed1a0c6cb7440fccec5ec2d1aa4f5fe12755d2e9
mehmetkose <mehmetkose122@gmail.com>
2016-01-08 23:22:04 +0200

    blog init

---
+++ content/posts/hello.md
@@ -0,0 +1,22 @@
+Title: Welcome All
+Slug: welcome-all
+Date: 2015-01-22 19:19
+Category: Python
+Tags: sdü, yazılım, sdüyazılım
+Author: Mehmet Köse
+Summary: Short version for index and feeds
+
+# Selam gençler bu bir blog post
+
+bu da yeni bir satır.
+
+
+muieaylmkuealmueia
+uei
+a
+uei
+au
+iea
+
+uie
+auiea
---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+AUTHOR = 'Alexis Métaireau'
+SITENAME = "Alexis' log"
+SITEURL = 'http://localhost:8000'
+TIMEZONE = "Europe/Paris"
+
+# can be useful in development, but set to False when you're ready to publish
+RELATIVE_URLS = True
+
+
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+
+LINKS = (('Biologeek', 'http://biologeek.org'),
+         ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
+
+SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
+          ('lastfm', 'http://lastfm.com/user/akounet'),)
+
+# path-specific metadata
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    }
+
+# static paths will be copied without parsing their contents
+STATIC_PATHS = [
+    'pictures',
+    'extra/robots.txt',
+    ]
+
